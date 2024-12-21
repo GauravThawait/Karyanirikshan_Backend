f5a5abe61ab04d603a214b2b8857e5923afb5411
Om Yadav <omyadav04352@gmail.com>
2024-12-21 16:35:19 +0530

    Lambda function integration tests.

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
-//import app from './app.js'
+import app from './src/app.js'
 import dotenv from 'dotenv'
 import dbClient from './src/db/connectDb.js'
 import serverless from 'serverless-http';
-
-import express from 'express'
-import errorHandler from './src/middleware/errorHandler.js'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
-
-//import routes
-import userRouterV1 from './src/routes/v1/user.routes.js'
-import departmentRouterV1 from './src/routes/v1/department.routes.js'
-import registerRouterV1 from './src/routes/v1/register.routes.js'
-import documentRouterV1 from './src/routes/v1/document.routes.js'
-import transferRouterV1 from './src/routes/v1/transfer.routes.js'
-import logsRouterV1 from './src/routes/v1/logs.routes.js'
-import workStatusRouterV1 from './src/routes/v1/workstatus.routes.js'
-import categoryRouterV1 from './src/routes/v1/category.routes.js'
 
 dotenv.config()
 
@@ -27,36 +12,7 @@ dbClient.connect()
     .catch(err => {
         console.error('Connection error', err.stack)
     })
-    
-const app = express()
-    
-const corsOptions = {
-    origin : process.env.CLIENT_URL || '*',
-    credentials : true,
-    allowedHeaders: ['Content-Type', 'Authorization']
-}
-    
-//middleware
-app.use(express.json())
-app.use(cors(corsOptions))
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended : true, limit: "16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
- 
-    
-app.use("/api/v1/user", userRouterV1)
-app.use("/api/v1/department", departmentRouterV1)
-app.use("/api/v1/register", registerRouterV1)
-app.use('/api/v1/document', documentRouterV1)
-app.use('/api/v1/transfer',transferRouterV1)
-app.use("/api/v1/log", logsRouterV1)
-app.use("/api/v1/workstatus", workStatusRouterV1)
-app.use("/api/v1/category", categoryRouterV1)
-    
-    //Error handler
-app.use(errorHandler);
-    
+
 app.get("/", (req, res) => {
     res.send("Server is running")
 })
@@ -64,6 +20,5 @@ app.get("/", (req, res) => {
 app.listen(process.env.PORT || 8000, () => {
     console.log(`Server listening on PORT ${process.env.PORT}`)
 })
-
-// Export the handler for AWS Lambda-test
+// Export the handler for AWS Lambda
 export const handler = serverless(app);
