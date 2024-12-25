26847cc94b1b891ce03987021bee75a806087170
Om Yadav <omyadav04352@gmail.com>
2024-12-25 14:41:27 +0530

    Testing triggers for staging.

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ app.get("/", (req, res) => {
 app.listen(process.env.PORT || 8000, () => {
     console.log(`Server listening on PORT ${process.env.PORT}`)
 })
-// Export the handler for AWS test for prod triggers.
+// Export the handler for AWS test for staging triggers.
 export const handler = serverless(app);
