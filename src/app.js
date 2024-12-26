162f42b21438f5902eaff00af17659830b48cd64
Gaurav Thawait <iamgauravthawait@gmail.com>
2024-12-26 12:07:52 +0530

    cors option set

--- src/app.js
+++ src/app.js
@@ -7,8 +7,23 @@ import auth from './middleware/auth.js'
 
 const app = express()
 
+
+//this is for dev purpose remove in future
+const allowedOrigins = [
+    process.env.CLIENT_URL_DEV, 
+    process.env.CLIENT_URL_STAGE, 
+    process.env.CLIENT_URL_PROD
+]
+
 const corsOptions = {
-    origin : process.env.CLIENT_URL,
+    origin : (origin, callback) => {
+        if(allowedOrigins.includes(origin)){
+            callback(null, true)
+        }
+        else{
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
     credentials : true,
     allowedHeaders: ['Content-Type', 'Authorization']
 }
